fbc01af4e89ea774aec6a787028d6e96d1852623
Danny Vaca <danny270793@icloud.com>
2026-07-16 14:06:08 -0500

    feat(gradle): extract Gradle cleaner into its own file

---
+++ gradle.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Gradle struct{}
+
+func (Gradle) Clean() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("failed to resolve home directory:", err)
+		return
+	}
+
+	removeContents(filepath.Join(home, ".gradle", "caches"))
+	removeContents(filepath.Join(home, ".gradle", "wrapper", "dists"))
+}
