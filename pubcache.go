96c83f7515a11be3fcea00ab86777206b01108b2
Danny Vaca <danny270793@icloud.com>
2026-07-16 14:06:15 -0500

    feat(pubcache): extract Pub Cache cleaner into its own file

---
+++ pubcache.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type PubCache struct{}
+
+func (PubCache) Clean() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("failed to resolve home directory:", err)
+		return
+	}
+
+	removeContents(filepath.Join(home, ".pub-cache"))
+}
