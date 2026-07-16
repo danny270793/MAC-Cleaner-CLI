9c456a25875f576fdf92b0c1338a14503902b116
Danny Vaca <danny270793@icloud.com>
2026-07-16 14:06:18 -0500

    feat(docker): extract Docker cleaner into its own file

---
+++ docker.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type Docker struct{}
+
+func (Docker) Clean() {
+	if _, err := exec.LookPath("docker"); err != nil {
+		fmt.Println("docker not installed (nothing to clean)")
+		return
+	}
+
+	cmd := exec.Command("docker", "system", "prune")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("failed to run docker system prune:", err)
+	}
+}
