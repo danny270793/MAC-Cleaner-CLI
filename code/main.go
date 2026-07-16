3a99f42901e353032c26483bfd7b572a45bc573b
Danny Vaca <danny270793@icloud.com>
2026-07-16 15:30:50 -0500

    feat(main): wire up new cleaner flags

--- code/main.go
+++ code/main.go
@@ -27,6 +27,12 @@ func main() {
 	libraryCaches := flag.Bool("library-caches", false, "clean ~/Library/Caches")
 	pubCache := flag.Bool("pub-cache", false, "clean ~/.pub-cache")
 	vscodeExtensions := flag.Bool("vscode-extensions", false, "remove outdated versions of ~/.vscode/extensions")
+	xcodeDerivedData := flag.Bool("xcode-derived-data", false, "clean ~/Library/Developer/Xcode/DerivedData")
+	coreSimulatorCaches := flag.Bool("core-simulator-caches", false, "clean ~/Library/Developer/CoreSimulator/Caches")
+	goModCache := flag.Bool("go-mod-cache", false, "clean the go module cache (go clean -modcache)")
+	cargoCache := flag.Bool("cargo-cache", false, "clean ~/.cargo/registry/cache and ~/.cargo/registry/src")
+	npmCache := flag.Bool("npm-cache", false, "clean ~/.npm")
+	pnpmStore := flag.Bool("pnpm-store", false, "prune the pnpm store (pnpm store prune)")
 	flag.Parse()
 
 	if *showVersion {
@@ -36,7 +42,19 @@ func main() {
 
 	var selected []Cleaner
 	if *all {
-		selected = []Cleaner{cleaners.Gradle{}, cleaners.LibraryCaches{}, cleaners.PubCache{}, cleaners.VSCodeExtensions{}, cleaners.Docker{}}
+		selected = []Cleaner{
+			cleaners.Gradle{},
+			cleaners.LibraryCaches{},
+			cleaners.PubCache{},
+			cleaners.VSCodeExtensions{},
+			cleaners.XcodeDerivedData{},
+			cleaners.CoreSimulatorCaches{},
+			cleaners.GoModCache{},
+			cleaners.CargoCache{},
+			cleaners.NpmCache{},
+			cleaners.PnpmStore{},
+			cleaners.Docker{},
+		}
 	} else {
 		if *gradle {
 			selected = append(selected, cleaners.Gradle{})
@@ -50,13 +68,31 @@ func main() {
 		if *vscodeExtensions {
 			selected = append(selected, cleaners.VSCodeExtensions{})
 		}
+		if *xcodeDerivedData {
+			selected = append(selected, cleaners.XcodeDerivedData{})
+		}
+		if *coreSimulatorCaches {
+			selected = append(selected, cleaners.CoreSimulatorCaches{})
+		}
+		if *goModCache {
+			selected = append(selected, cleaners.GoModCache{})
+		}
+		if *cargoCache {
+			selected = append(selected, cleaners.CargoCache{})
+		}
+		if *npmCache {
+			selected = append(selected, cleaners.NpmCache{})
+		}
+		if *pnpmStore {
+			selected = append(selected, cleaners.PnpmStore{})
+		}
 		if *docker {
 			selected = append(selected, cleaners.Docker{})
 		}
 	}
 
 	if len(selected) == 0 {
-		fmt.Println("no cleaner selected, pass --all or one of --docker --gradle --library-caches --pub-cache --vscode-extensions")
+		fmt.Println("no cleaner selected, pass --all or one of --docker --gradle --library-caches --pub-cache --vscode-extensions --xcode-derived-data --core-simulator-caches --go-mod-cache --cargo-cache --npm-cache --pnpm-store")
 		flag.Usage()
 		return
 	}
