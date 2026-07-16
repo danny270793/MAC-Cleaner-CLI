dad779d91553cd01f736d998f0fc548e960ac984
Danny Vaca <danny270793@icloud.com>
2026-07-16 14:06:12 -0500

    feat(librarycaches): extract Library Caches cleaner into its own file

---
+++ librarycaches.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type LibraryCaches struct{}
+
+func (LibraryCaches) Clean() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("failed to resolve home directory:", err)
+		return
+	}
+
+	removeContents(filepath.Join(home, "Library", "Caches"))
+}
